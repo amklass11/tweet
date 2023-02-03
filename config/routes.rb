aa4067e9bcdc84ec9ba2c535cc91b08e3d1475be
amklass11 <lassanaa14@gmail.com>
2023-02-03 10:12:54 +0100

    databaes final setup and auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :tweeets
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "tweeets#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
